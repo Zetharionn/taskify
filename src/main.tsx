9e19eb1eb7224eee0f1fe8ca045cb1982ab0e1c3
Zetharion <zetharionshim@gmail.com>
2024-06-11 02:14:53 +0300

    feat: home page

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Home } from '@/pages'
 
-const router = createBrowserRouter([])
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />
+	}
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
