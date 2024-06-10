baa846af04cbe745abe1f0ccdd5ae45eb62a4c22
Zetharion <zetharionshim@gmail.com>
2024-06-11 02:10:47 +0300

    feat: routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode></React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 )
