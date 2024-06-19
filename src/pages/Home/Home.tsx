d4bdcc33b093d05560beebda280096d268da8815
Zetharion <zetharionshim@gmail.com>
2024-06-19 14:27:36 +0300

    feat: logo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,17 @@
 import { FC } from 'react'
+import { Box } from 'lucide-react'
 
 export const Home: FC = () => {
 	return (
-		<div>
-			<h1 className='text-3xl'>Home Page</h1>
-		</div>
+		<>
+			<div className='flex'>
+				<div className='flex flex-col h-screen'>
+					<div className='flex flex-row items-center p-1'>
+						<Box />
+						<h1 className='text-2xl font-semibold'>Taskify</h1>
+					</div>
+				</div>
+			</div>
+		</>
 	)
 }
