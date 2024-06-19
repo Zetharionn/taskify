92f45c7f673fb7609dbb722bd858ab9f3c5d58b9
Zetharion <zetharionshim@gmail.com>
2024-06-19 11:25:09 +0300

    fix: deleted utils and useless import shorteners

--- src/utils/hooks/useTasks.ts
+++
@@ -1,9 +0,0 @@
-import { useTasksStore } from '@/store/Tasks'
-
-export const useTasks = () => {
-	const tasks = useTasksStore(state => state.tasks)
-	const useCreateTask = useTasksStore(state => state.createTask)
-	const useDeleteTask = useTasksStore(state => state.deleteTask)
-
-	return { tasks, useCreateTask, useDeleteTask }
-}
