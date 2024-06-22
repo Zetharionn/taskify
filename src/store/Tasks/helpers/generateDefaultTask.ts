d771bb72586d2e0cf1f6c55d4e7633438ef05f98
Zetharion <zetharionshim@gmail.com>
2024-06-22 12:36:32 +0300

    refactor: simplify helper

--- src/store/Tasks/helpers/generateDefaultTask.ts
+++ src/store/Tasks/helpers/generateDefaultTask.ts
@@ -1,12 +1,10 @@
 import { EnumPriority, EnumStatus, ITask } from '@store/Tasks/Tasks.types.ts'
 import { v4 as uuidv4 } from 'uuid'
 
-export const generateDefaultTask = (): ITask => {
-	return {
-		id: uuidv4(),
-		title: 'Untitled',
-		body: '',
-		priority: EnumPriority.Medium,
-		status: EnumStatus.Todo
-	}
-}
+export const generateDefaultTask = (): ITask => ({
+	id: uuidv4(),
+	title: 'Untitled',
+	body: '',
+	priority: EnumPriority.Medium,
+	status: EnumStatus.Todo
+})
